7ceaac08530a2b1ba2fc78ce581e526e7b7d9b86
xisto eugenio <xistoeugeniosilva@gmail.com>
2022-10-04 13:13:40 -0300

    design template created

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './app.scss';
+export default function App() {
+  return (
+    <div className="App">
+      <div className="topbar">jkhskgh</div>
+      <div className="sidebar">dhdhgffbfg</div>
+      <section className='sections'>
+        <div className='section pink'>
+          <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+        <div className='section yellow'>
+        <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+        <div className='section blue'>
+        <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+        <div className='section pink'>
+          <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+        <div className='section yellow'>
+        <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+        <div className='section blue'>
+        <div className="content">
+            nfdjngklsh
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
