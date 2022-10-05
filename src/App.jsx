9e9578caa7e8f982bcbac9c5f23f1ef61e2407f8
xisto eugenio <xistoeugeniosilva@gmail.com>
2022-10-05 14:27:48 -0300

    added profile in page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import './app.scss';
+import Profile from './components/profile/Profile';
+
 export default function App() {
   return (
     <div className="App">
       <div className="topbar">jkhskgh</div>
       <div className="sidebar">dhdhgffbfg</div>
       <section className='sections'>
-        <div className='section pink'>
-          <div className="content">
-            nfdjngklsh
-          </div>
-        </div>
-        <div className='section yellow'>
-        <div className="content">
-            nfdjngklsh
-          </div>
-        </div>
-        <div className='section blue'>
-        <div className="content">
-            nfdjngklsh
-          </div>
+        <div className="section">
+          <Profile />
         </div>
         <div className='section pink'>
           <div className="content">
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,17 @@
+import "./profile.scss";
+
+export default function Profile() {
+  return (
+    <div className="profile">
+        <div className="profileContainer">
+            <div className="imgContainer">
+                <img src="assets/xisto.jpg" alt="" />
+            </div>
+            <div className="textContainer">
+                <h1>Hi, I am Xisto Eugênio</h1>
+                <span>and I am a front-end developer</span>
+            </div>
+        </div>
+    </div>
+  )
+}
